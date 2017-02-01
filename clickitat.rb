77d49dbfcb452dc847e0600aee5a9485ae65ccbc
Eric Ebbesen <eebbesen.git@gmail.com>
2017-02-01 07:14:56 -0600

    rubocop-y things

--- clickitat.rb
+++ clickitat.rb
@@ -1,18 +1,17 @@
-
-require "cuba"
-require "mote"
-require "mote/render"
+require 'cuba'
+require 'mote'
+require 'mote/render'
 
 Cuba.plugin(Mote::Render)
 
 Cuba.use Rack::Static,
-  root: "public",
-  urls: ["/css","/js", "/fonts"]
+  root: 'public',
+  urls: ['/css','/js', '/fonts']
 
 Cuba.define do
   on get do
     on root do
-      res.write view("home", {title: "Cuba Genie"})
+      res.write view('home', {title: 'Cuba Genie'})
     end
     on 'openclick' do
       res.write 'Opening'
